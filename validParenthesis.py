432bc1415689a9f056378b40f645e1f2927a924e
= <teachlife@outlook.com>
2020-06-20 15:14:43 -0400

    refactors parenthesis to include dictionary

--- validParenthesis.py
+++ validParenthesis.py
@@ -4,30 +4,21 @@ def validParenthesis(string):
     lst = []
     lst[:]= string
     print(lst)
+    parens = {"(": ")", "{": "}", "[":"]"}
     opn = ["(", "{", "["]
     for char in lst:
         if char in opn:
             queue.append(char)
-        elif queue[-1] == "(":
-            if char == ")":
-                queue.pop()
-            else:
-                return False
-        elif queue[-1] == "{":
-            if char == "}":
-                queue.pop()
-            else:
-                return False            
-        elif queue[-1] == "[":
-            if char == "]":
-                queue.pop()
-            else:
-                return False
-
+        elif queue == 0:
+            return False
+        elif char != parens[queue[-1]]:
+            return False
+        elif char == parens[queue[-1]]:
+            queue.pop()
     if len(queue) != 0:
         return False
     return True
 
 
-string_par= "({{}{]}})"
+string_par= "({{}{[]}})"
 print(validParenthesis(string_par))
